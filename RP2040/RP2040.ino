f67e94c64083deb9a817d661a27fd21cbf22c47f
Npaintrobo <143476336+Npaintrobo@users.noreply.github.com>
2024-12-06 21:17:11 +0900

    RP2040.ino を更新

--- RP2040/RP2040.ino
+++ RP2040/RP2040.ino
@@ -1,5 +1,16 @@
+
+const int PWM5_1 = 0;
+const int PWM5_2 = 1;
+const int PWM6_1 = 2;
+const int PWM6_2 = 3;
+const int PWM7_1 = 4;
+const int PWM7_2 = 5;
+const int PWM8_1 = 6;
+const int PWM8_2 = 7;
+
 void setup() {
   Serial.begin(115200);
+  pinMode(
 }
 
 void loop() {
